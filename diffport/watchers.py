596f49b4e57da23056391ee3f78bfdd382e1da51
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-06-24 00:08:27 -0400

    schema table watcher

--- diffport/watchers.py
+++ diffport/watchers.py
@@ -37,8 +37,33 @@ class WatcherTablesInSchema(Watcher):
 
     @staticmethod
     def take_snapshot(db, config):
-        return 0
+        """
+        Save list of tables in given schema
+        """
+
+        res = db.query("SELECT table_name FROM information_schema.tables WHERE table_schema = '{}'".format(config["schema"]))
+        return [r["table_name"] for r in res]
 
     @staticmethod
     def diff(a, b):
-        pass
+        """
+        Assume b to be the newer one
+        """
+
+        removed = list(set(a) - set(b))
+        added = list(set(b) - set(a))
+
+        out = ""
+        if len(removed) != 0:
+            out += "Removed tables :\n\n"
+            for tb in removed:
+                out += "- {}\n".format(tb)
+            out += "\n\n"
+
+        if len(added) != 0:
+            out += "Added tables :\n\n"
+            for tb in added:
+                out += "- {}\n".format(tb)
+            out += "\n\n"
+
+        return out
