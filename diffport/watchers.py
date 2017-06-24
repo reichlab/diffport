5d63fc09af73a77f145497a658373f7cf9a8eb94
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-06-24 01:08:53 -0400

    Add grouped row count diffs

--- diffport/watchers.py
+++ diffport/watchers.py
@@ -2,7 +2,9 @@
 Modules for watchers
 """
 
+import pandas as pd
 from abc import ABC, abstractmethod
+from tabulate import tabulate
 
 # Exceptions
 class WatcherNotImplemented(Exception):
@@ -27,11 +29,37 @@ class WatcherNumberOfRows(Watcher):
 
     @staticmethod
     def take_snapshot(db, config):
-        return db[config["table"]].count()
+        """
+        Take snapshot for number of rows in given table grouped by asked fields
+        """
+
+        if len(config["groupby"]) > 0:
+            stmt = "SELECT {0}, count(*) AS count FROM {1} GROUP BY {0} ORDER BY {0}".format("', ".join(config["groupby"]), config["table"])
+            return [r for r in db.query()]
+        else:
+            # Total count
+            return db[config["table"]].count()
 
     @staticmethod
     def diff(a, b):
-        pass
+        # Check if both have same type of snapshot
+        assert type(a) == type(b)
+
+        if type(a) is int:
+            return "Change: {} rows".format(b - a)
+        else:
+            a = pd.DataFrame(a)
+            b = pd.DataFrame(b)
+            merge_keys = list(a.columns[:-1])
+            merged = pd.merge(a, b, how="outer", on=merge_keys)
+            merged["count diff"] = merged["count_x"] - merged["count_y"]
+            del merged["count_x"]
+            del merged["count_y"]
+
+            out = "Changes: \n\n"
+            out += tabulate(merged, headers="keys")
+            out += "\n\n"
+            return out
 
 class WatcherTablesInSchema(Watcher):
 
